19c6ea4d0930e1f9598e8db903530aa5a1324d5e
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-08 12:24:23 +0530

    direct linking done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div>
          <nav className="navbar navbar-expand-lg bg-warning">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">ALBUM</a>
+                    <a className="navbar-brand" href="#">SHOPPINGCART</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
